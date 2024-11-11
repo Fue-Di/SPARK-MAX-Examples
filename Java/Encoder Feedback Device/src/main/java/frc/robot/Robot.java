c008b5cccee69fbbc85cf3c165eebd8d6be5c706
Freddy <freddy@revrobotics.com>
2024-11-11 10:57:38 -0600

    Update example code to use REVLib 2025

--- Java/Encoder Feedback Device/src/main/java/frc/robot/Robot.java
+++ Java/Encoder Feedback Device/src/main/java/frc/robot/Robot.java
@@ -11,65 +11,76 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkPIDController;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkRelativeEncoder;
-import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 
 public class Robot extends TimedRobot {
-  private static final int deviceID = 0;
-  private CANSparkMax m_motor;
-  private SparkPIDController m_pidController;
+  private static final int deviceID = 1;
+  private SparkMax motor;
+  private SparkMaxConfig motorConfig;
+  private SparkClosedLoopController closedLoopController;
   public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
 
   /**
    * A RelativeEncoder object is constructed using the GetEncoder() method on an 
-   * existing CANSparkMax object. The assumed encoder type is the hall effect,
+   * existing SparkMax object. The assumed encoder type is the hall effect,
    * or a sensor type and counts per revolution can be passed in to specify
-   * a different kind of sensor. Here, it's a quadrature encoder with 4096 CPR.
+   * a different kind of sensor. Here, it's a quadrature encoder with 8192 CPR.
    */
-  private RelativeEncoder m_encoder;
+  private RelativeEncoder encoder;
 
   @Override
   public void robotInit() {
-    // initialize SPARK MAX with CAN ID
-    m_motor = new CANSparkMax(deviceID, MotorType.kBrushed);
-    m_encoder = m_motor.getEncoder(SparkRelativeEncoder.Type.kQuadrature, 4096);
-    
-    m_motor.restoreFactoryDefaults();
+    // Initialize SPARK MAX with CAN ID 1
+    motor = new SparkMax(deviceID, MotorType.kBrushless);
+
+    // Initialize SPARK MAX Configuration with PID changes
+    motorConfig = new SparkMaxConfig();
 
     /**
-     * In order to use PID functionality for a controller, a SparkPIDController object
-     * is constructed by calling the getPIDController() method on an existing
-     * CANSparkMax object
+     * The PID Controller can be configured to use the primary encoder sensor as its feedback
+     * device with the method feedbackDevice() 
      */
-    m_pidController = m_motor.getPIDController();
-  
+
+    // Define PID coefficients
+    kP = 0.008;
+    kI = 0.0000001;
+    kD = 0.00001;
+    kIz = 0;
+    kFF = 0.00001;
+    kMaxOutput = 0.5;
+    kMinOutput = -0.5;
+
+    // Set PID coefficients in configuration
+    motorConfig.closedLoop
+      .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+      .p(kP)
+      .i(kI)
+      .d(kD)
+      .iZone(kIz)
+      .velocityFF(kFF)
+      .outputRange(kMinOutput, kMaxOutput);
+
+    // Restore defaults and apply configuration changes to the motor
+    motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+
+    // Get Encoder object
+    encoder = motor.getEncoder();
+
     /**
-     * The PID Controller can be configured to use the analog sensor as its feedback
-     * device with the method SetFeedbackDevice() and passing the PID Controller
-     * the CANAnalog object. 
+     * In order to use Closed Loop functionality for a controller, a SparkClosedLoopController object
+     * is constructed by calling the getClosedLoopController() method on an existing
+     * SparkMax object
      */
-    m_pidController.setFeedbackDevice(m_encoder);
+    closedLoopController = motor.getClosedLoopController();
 
-    // PID coefficients
-    kP = 0.1; 
-    kI = 1e-4;
-    kD = 1; 
-    kIz = 0; 
-    kFF = 0; 
-    kMaxOutput = 1; 
-    kMinOutput = -1;
-
-    // set PID coefficients
-    m_pidController.setP(kP);
-    m_pidController.setI(kI);
-    m_pidController.setD(kD);
-    m_pidController.setIZone(kIz);
-    m_pidController.setFF(kFF);
-    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
-
-    // display PID coefficients on SmartDashboard
+    // Display PID coefficients on SmartDashboard
     SmartDashboard.putNumber("P Gain", kP);
     SmartDashboard.putNumber("I Gain", kI);
     SmartDashboard.putNumber("D Gain", kD);
@@ -82,7 +93,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    // read PID coefficients from SmartDashboard
+    // Read PID coefficients from SmartDashboard
     double p = SmartDashboard.getNumber("P Gain", 0);
     double i = SmartDashboard.getNumber("I Gain", 0);
     double d = SmartDashboard.getNumber("D Gain", 0);
@@ -92,19 +103,21 @@ public class Robot extends TimedRobot {
     double min = SmartDashboard.getNumber("Min Output", 0);
     double rotations = SmartDashboard.getNumber("Set Rotations", 0);
 
-    // if PID coefficients on SmartDashboard have changed, write new values to controller
-    if((p != kP)) { m_pidController.setP(p); kP = p; }
-    if((i != kI)) { m_pidController.setI(i); kI = i; }
-    if((d != kD)) { m_pidController.setD(d); kD = d; }
-    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
-    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
+    // If PID coefficients on SmartDashboard have changed, write new values to controller
+    if((p != kP)) { motorConfig.closedLoop.p(p); kP = p; }
+    if((i != kI)) { motorConfig.closedLoop.i(i); kI = i; }
+    if((d != kD)) { motorConfig.closedLoop.d(d); kD = d; }
+    if((iz != kIz)) { motorConfig.closedLoop.iZone(iz); kIz = iz; }
+    if((ff != kFF)) { motorConfig.closedLoop.velocityFF(ff); kFF = ff; }
     if((max != kMaxOutput) || (min != kMinOutput)) { 
-      m_pidController.setOutputRange(min, max); 
+      motorConfig.closedLoop.outputRange(min, max);
       kMinOutput = min; kMaxOutput = max; 
     }
 
+    // Update PID value changes if any to the motor
+    motor.configure(motorConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
     /**
-     * PIDController objects are commanded to a set point using the 
+     * ClosedLoopController objects are commanded to a set point using the 
      * SetReference() method.
      * 
      * The first parameter is the value of the set point, whose units vary
@@ -112,14 +125,14 @@ public class Robot extends TimedRobot {
      * 
      * The second parameter is the control type can be set to one of four 
      * parameters:
-     *  com.revrobotics.CANSparkMax.ControlType.kDutyCycle
-     *  com.revrobotics.CANSparkMax.ControlType.kPosition
-     *  com.revrobotics.CANSparkMax.ControlType.kVelocity
-     *  com.revrobotics.CANSparkMax.ControlType.kVoltage
+     *  com.revrobotics.spark.SparkBase.ControlType.kDutyCycle
+     *  com.revrobotics.spark.SparkBase.ControlType.kPosition
+     *  com.revrobotics.spark.SparkBase.ControlType.kVelocity
+     *  com.revrobotics.spark.SparkBase.ControlType.kVoltage
      */
-    m_pidController.setReference(rotations, CANSparkMax.ControlType.kPosition);
+    closedLoopController.setReference(rotations, SparkMax.ControlType.kPosition);
     
     SmartDashboard.putNumber("SetPoint", rotations);
-    SmartDashboard.putNumber("ProcessVariable", m_encoder.getPosition());
+    SmartDashboard.putNumber("ProcessVariable", encoder.getPosition());
   }
 }
